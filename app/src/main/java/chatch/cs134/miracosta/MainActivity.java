8411469ddf481bdb7990d59a02881c0356d43516
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-02-28 14:56:31 -0800

    Add new class TaskListAdapter

--- app/src/main/java/chatch/cs134/miracosta/MainActivity.java
+++ app/src/main/java/chatch/cs134/miracosta/MainActivity.java
@@ -3,11 +3,16 @@ package chatch.cs134.miracosta;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 import chatch.cs134.miracosta.Model.DBHelper;
+import chatch.cs134.miracosta.Model.Task;
 
 public class MainActivity extends AppCompatActivity {
 
     private DBHelper mDb;
+    private List<Task> mAllTasks;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Instantiate (create) the database
         mDb = new DBHelper(this);
+
+        // FIll list with data from the database
+        mAllTasks = mDb.getAllTasks();
     }
 
     // Ctrl + o => overrides
---
+++ app/src/main/java/chatch/cs134/miracosta/TaskListAdapter.java
@@ -0,0 +1,4 @@
+package chatch.cs134.miracosta;
+
+public class TaskListAdapter {
+}
